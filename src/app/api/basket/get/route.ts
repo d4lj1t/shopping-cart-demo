c6d32f90acbcb3cb7f8992f4c845debc28de845d
DAL <d4lj1t@hotmail.com>
2024-01-08 14:38:52 +0000

    basket fetched from database

--- src/app/api/basket/get/route.ts
+++ src/app/api/basket/get/route.ts
@@ -4,8 +4,8 @@ import connectToMongoDb from '@/app/libs/mongodb';
 import basketModel from '@/app/models/basket';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export async function GET(request: {json: () => Promise<Product>}) {
+export async function GET(request: {json: () => Promise<Product[]>}) {
 	await connectToMongoDb();
-	const basketItems = await basketModel.find();
-	return NextResponse.json({basketItems});
+	const data = await basketModel.find();
+	return NextResponse.json(data);
 }
--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -1,33 +1,54 @@
-'use client';
-
-import React, {useContext} from 'react';
+import React from 'react';
 import Link from 'next/link';
-import {Context} from '@/app/context';
-import {type Product, type ContextType} from '@/app/types';
+import {type Product} from '@/app/types';
 import styles from '@/app/components/Products/index.module.scss';
+import {baseApiUrl} from '@/app/constants';
 
-export default function Basket() {
-	const {cartItems, removeFromCart} = useContext(Context) as ContextType;
+type ErrorResponse = {
+	error: string;
+};
+
+async function getBasket(): Promise<Product[] | ErrorResponse> {
+	try {
+		const res = await fetch(`${baseApiUrl}/api/basket/get`);
+
+		if (!res.ok) {
+			throw new Error(`HTTP error! Status: ${res.status}`);
+		}
+
+		return await res.json() as Product[];
+	} catch (error) {
+		console.error('Error in getBasket:', error);
+		const errorMessage = error instanceof Error && error.message ? error.message : 'Failed to fetch basket';
+		const errorResponse: ErrorResponse = {error: errorMessage};
+
+		return errorResponse;
+	}
+}
+
+export default async function Basket() {
+	const basketItems: Product[] | ErrorResponse = await getBasket();
+
+	if ('error' in basketItems) {
+		return <p>Error: {basketItems.error}</p>;
+	}
 
 	return (
 		<>
 			<Link href='/home' className='flex  flex-col items-center'>Home</Link>
 			<div className='flex flex-wrap gap-4 p-5'>
-				{cartItems && cartItems.length > 0
-					&& cartItems.map((item: Product, index: number) => (
+				{basketItems.length > 0
+					&& basketItems.map((item: Product, index: number) => (
 						<div className={styles.item} key={index}>
 							<h1 className={styles.item__title}>{item.title}</h1>
 							<h1>
 								{item.currency}
 								{item.priceValue}
 							</h1>
-							<button onClick={() => {
-								removeFromCart(index);
-							}} className='p-2 bg-blue-200 cursor-pointer mt-5'>Remove from basket</button>
 						</div>
 					))
 				}
-				{!cartItems.length && <div>No items added</div>}
+				{!basketItems.length && <div>No items added</div>}
 			</div>
 		</>
 	);
